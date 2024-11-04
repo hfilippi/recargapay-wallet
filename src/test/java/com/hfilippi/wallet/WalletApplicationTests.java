31cd3b2f21551d62e6c47bb05b03da919dfc7640
hfilippi <hfilippi@gmail.com>
2024-11-03 21:47:22 -0300

    Test for contextLoads()

--- src/test/java/com/hfilippi/wallet/WalletApplicationTests.java
+++ src/test/java/com/hfilippi/wallet/WalletApplicationTests.java
@@ -1,13 +1,27 @@
 package com.hfilippi.wallet;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.hfilippi.wallet.controller.WalletController;
+import com.hfilippi.wallet.service.WalletService;
 
 @SpringBootTest
 class WalletApplicationTests {
 
+	@Autowired
+	private WalletController walletController;
+
+	@Autowired
+	private WalletService walletService;
+
 	@Test
 	void contextLoads() {
+		assertThat(walletController).isNotNull();
+		assertThat(walletService).isNotNull();
 	}
 
 }
